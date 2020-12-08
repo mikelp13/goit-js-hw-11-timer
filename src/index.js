e2867327c2dc2f3c36443ee40a6e594fe8e8aa97
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-12-08 20:30:45 +0200

    added fixed timer

--- src/index.js
+++ src/index.js
@@ -1,37 +1,41 @@
 import './styles.css';
-import refs from './refs'
 
 class CountdownTimer {
 
   constructor({ selector, targetDate }) {
     this.timer = document.querySelector(selector);
     this.targetDate = targetDate;
-
+    this.daysValue = this.timer.querySelector('[data-value="days"]');
+    this.hoursValue = this.timer.querySelector('[data-value="hours"]');
+    this.minsValue = this.timer.querySelector('[data-value="mins"]');
+    this.secsValue = this.timer.querySelector('[data-value="secs"]');
   }
 
-   getTimeRemaining(time) {
+  getTimeRemaining(time) {
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-    const hours = this.pad( Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-    refs.daysValue.textContent = `${days}`;
-    refs.hoursValue.textContent = `${hours}`;
-    refs.minsValue.textContent = `${mins}`;
-    refs.secsValue.textContent = `${secs}`;
-   }
 
+    this.daysValue.textContent = `${days}`;
+    this.hoursValue.textContent = `${hours}`;
+    this.minsValue.textContent = `${mins}`;
+    this.secsValue.textContent = `${secs}`;
+  }
+ 
   setTimer = setInterval(() => {
-    const currentDate = new Date();
-    const time = Date.parse(this.targetDate) - Date.parse(currentDate);
+      const currentDate = new Date();
+      const time = Date.parse(this.targetDate) - Date.parse(currentDate);
 
-    this.endOfTime(time);
+      this.endOfTime(time);
 
-    this.getTimeRemaining(time);
-
-  }, 1000);
+      this.getTimeRemaining(time);
+    }, 1000);
 
   pad(value) {
-    return String(value).padStart(2, "0"); // add 0 to number '1' --> '01'
+    return String(value).padStart(2, '0'); // add 0 to number '1' --> '01'
   }
 
   endOfTime(time) {
@@ -43,8 +47,14 @@ class CountdownTimer {
 }
 
 const newTimer = new CountdownTimer({
-  selector: "#timer-1",
-  targetDate: new Date("Dec 28, 2020"),
+  selector: '#timer-1',
+  targetDate: new Date('Dec 28, 2020'),
 });
+
+const newTimer2 = new CountdownTimer({
+  selector: '#timer-2',
+  targetDate: new Date('Jan 11, 2021'),
+});
+
 
 
--- src/refs.js
+++
@@ -1,6 +0,0 @@
-export default {
-  daysValue: document.querySelector('[data-value="days"]'),
-  hoursValue: document.querySelector('[data-value="hours"]'),
-  minsValue: document.querySelector('[data-value="mins"]'),
-  secsValue: document.querySelector('[data-value="secs"]'),
-};
